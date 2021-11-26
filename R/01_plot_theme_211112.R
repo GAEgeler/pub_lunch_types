7d8c772b8b96ff65b3bcc7a9fdde6d4013227ada
Egeler Gian-Andrea (egel) <egel@zhaw.ch>
2021-11-26 17:27:07 +0100

    changes in export figures, issue color blindness

--- R/01_plot_theme_211112.R
+++ R/01_plot_theme_211112.R
@@ -15,6 +15,13 @@ ColsPerCat =  c("buffet" = "#e64d00",
                 "meat lover" = "#fad60d",
                 "always meat" = "#262626")
 
+ColsPerCat_blind =  c("buffet" = "#D81B60", #should be rather alwys meat
+                "never meat" = "#1E88E5",
+                "veg-flexitarian" = "#FFC107", 
+                "meat-flexitarian" = "#004D40",
+                "meat-eater" = "#AAED89" , 
+                "meat lover" = "#F59D28",
+                "always meat" = "#5D6395")
 
 #define names for food types
 NamesPerCat = tibble::tibble(food_type = c("buffet", 
@@ -66,26 +73,29 @@ mytheme <- ggplot2::theme_bw()+ # definve theme for plot
 
 
 ### for plots with double x-axis: https://stackoverflow.com/questions/52554822/save-a-ggplot2-time-series-plot-grob-generated-by-ggplotgrob
-mytheme_facet <- ggplot2::theme_classic()+ # definve theme for plot
+mytheme_facet <- ggplot2::theme_classic()+ # define theme for plot
     ggplot2::theme(axis.text.x = ggplot2::element_text(size=22),
                    axis.text.y = ggplot2::element_text(size=22, face = "plain"),
-                   #remove the lines, background and grid
-                   # panel.border = ggplot2::element_blank(),
-                   # panel.background = ggplot2::element_blank(),
                    panel.grid.major = ggplot2::element_blank(),
-                   # panel.grid = ggplot2::element_blank(),
                    legend.text = ggplot2::element_text(size = 22),
                    legend.title = ggplot2::element_text(size =22),
                    strip.text = ggplot2::element_text(size=22),
-                   strip.background.x = ggplot2::element_blank(), #background color to blanc facet wraps
+                   strip.text.x = element_text(size = 22,  face = "bold",  
+                                               hjust = 0, 
+                                               margin = margin(.05, 0, .05, 0.05, "cm")),
+                   #background color to blank facet wraps
+                   strip.background.x = ggplot2::element_blank(), 
                    # remove facet spacing on x-direction
                    strip.placement = 'outside',
                    panel.spacing.x = ggplot2::unit(0, "lines"), 
+                   panel.spacing = unit(1, "lines"),
                    axis.title.y = ggplot2::element_text(size = 22, margin = ggplot2::margin(t = 0, r = 22, b = 0, l = 0)),
                    axis.title.x = ggplot2::element_text(size = 22,  margin = ggplot2::margin(t = 22, r = 0, b = 0, l = 0)),
                    plot.subtitle = ggplot2::element_text(margin = ggplot2::margin(b=15),size = 30),
                    plot.caption = ggplot2::element_text(margin = ggplot2::margin(t=15), face="italic", size=22),
-                   text = ggplot2::element_text(family = ggplot2::theme_get()$text$family)) 
+                   text = ggplot2::element_text(family = ggplot2::theme_get()$text$family), 
+                   legend.position = "bottom", 
+                   legend.key.size = unit(1, "cm"),) 
 
 #set coord expansion
 #source: https://stackoverflow.com/questions/61969752/force-the-origin-to-start-at-0-without-margin-between-data-and-x-axis-in-new-ggp
